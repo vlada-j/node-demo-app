79bc3236dafe2ddbe626bbb61e2148b8932da427
vlada-j <vladajanosevic@gmail.com>
2022-01-14 23:43:37 +0100

    app update

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ app.get("/", function (req, res) {
 	return res.send(`Server is running on ${startServerTime}`);
 });
 
-// listen to port 7000 by default
-app.listen(process.env.PORT || 7777, () => {
+app.listen(80, () => {
 	console.log(`Server is running on ${startServerTime}`);
 });
 
